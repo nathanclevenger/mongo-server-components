5281dff0f89d8a79e1492d7e20078d3943ecb4d8
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2024-01-31 02:22:55 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mongo-server-components
+
 React Server Components for MongoDB List + CRUD
---
+++ index.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import { Document, Filter, MatchKeysAndValues, MongoClient, Sort, SortDirection, WithId } from 'mongodb'
+
+const client = new MongoClient(process.env.MONGODB_URI!)
+
+
+type ListProps<T = Document> = {
+  id?: string
+  style?: React.CSSProperties
+  classNames?: string
+  children?: React.ReactElement<WithId<Document>>
+  db?: string
+  coll: string
+  filter?: Filter<T>
+  sort?: Sort
+  direction?: SortDirection
+  skip?: number
+  limit?: number
+  item?: React.FC<WithId<Document>>
+} & ({
+  children: React.ReactElement<WithId<Document>>
+} | {
+  item: React.FC<WithId<Document>>
+})
+
+
+export const MongoList = async ({ db, coll, filter = {}, sort, direction, skip, limit, item, children, ...props }: ListProps) => {
+
+  const cursor = client.db(db).collection(coll).find(filter)
+  if (sort) cursor.sort(sort, direction)
+  if (skip) cursor.skip(skip)
+  if (limit) cursor.limit(limit)
+  const data = await cursor.toArray()
+
+
+  if (children) return (
+    <div {...props}>
+      {data.map((doc, i) => React.cloneElement(children, { key: i, ...doc }))}
+    </div>
+  )
+
+  if (item) return (
+    <div {...props}>
+      {data.map((doc, i) => React.createElement(item, { key: i, ...doc }))}
+    </div>
+  )
+}
+
+
+type ItemProps<T = Document> = {
+  id?: string
+  style?: React.CSSProperties
+  classNames?: string
+  db?: string
+  coll: string
+  filter: Filter<T>
+  children?: React.ReactElement<WithId<T>>
+  item?: React.FC<WithId<T>>
+  create?: boolean
+  setOnInsert?: MatchKeysAndValues<T>
+} & ({
+  children: React.ReactElement<WithId<T>>
+} | {
+  item: React.FC<WithId<T>>
+})
+
+
+export const MongoItem = async ({ db, coll, filter, create, children, item }: ItemProps) => {
+
+  const doc = create 
+    ? await client.db(db).collection(coll).findOneAndUpdate(filter, { $setOnInsert: filter }, { upsert: true, returnDocument: 'after' }) 
+    : await client.db(db).collection(coll).findOne(filter)  
+
+  if (children && doc) return React.cloneElement(children, doc)
+  if (item && doc) return React.createElement(item, doc)
+
+}
+
+
+export const MongoForm = async ({ children, ...props }: ItemProps) => {
+
+  async function upsert(formData: FormData) {
+    'use server'
+    const data = Object.fromEntries(formData.entries())
+    const doc = await client.db(props.db).collection(props.coll).findOneAndUpdate(props.filter, 
+        { $setOnInsert: props.filter, $set: data }, 
+        { upsert: true, returnDocument: 'after' })
+  }
+
+  return (
+    <form action={upsert}>
+      {children}
+    </form>
+  )
+
+}
